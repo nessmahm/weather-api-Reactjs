3288592cb7a03a4e23b8fb5cb31bc48796c30663
Ness Hm <nessma.hamidou@insat.u-carthage.tn>
2022-12-20 14:58:38 +0100

    update

---
+++ src/components/Home.js
@@ -0,0 +1,75 @@
+import {React,useState} from 'react'
+import axios from "axios";
+
+function Home() {
+    const [location, setLocation] = useState('')
+    const [data, setData] = useState({})
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=731e961517486404c2ea774dede03efb&units=metric`
+    const searchLocation = (event) =>{
+     if(event.key =='Enter') {
+        axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(data)
+        
+        })
+     setLocation('')
+
+    }
+    
+    }
+ 
+    return (
+    <div className='home'>
+
+        <div className='location'> 
+            <input type = 'text' 
+                   placeholder='Enter Location' 
+                   value={location} 
+                   onChange = { (event) => setLocation(event.target.value)  }
+                   onKeyPress = {searchLocation}
+                   /> 
+        </div>
+
+        <div className='container'> 
+        
+            <div className='top'>
+                <div className='loc'>  
+                    <p>{data.name}</p>
+                </div>
+               
+                <div className='temp'> 
+                    <h1>{data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}</h1>
+                </div>
+                
+                <div className='descript'>
+                    <p> {data.weather ? <p>{data.weather[0].main}</p> : null}</p>
+                </div>
+           
+            </div>
+
+            <div className='bottom'>
+                <div className='feels'>
+                        <h3> {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null} </h3>
+                        <p>Fells Like</p>
+                </div>
+               
+                <div className='humidity'>
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}                    <p>Humidity</p>
+                </div>
+               
+                <div className='winds'>
+                    <h3>{data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null} </h3>
+                    <p>Wind Speed</p>
+                </div>
+
+            </div>
+        
+        </div>
+    
+    </div>
+
+  )
+}
+
+export default Home
